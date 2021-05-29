ac83069cf329901aab5192853478bef5bf06b029
jooms6992 <70081544+jooms6992@users.noreply.github.com>
2021-05-29 13:53:23 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -32,3 +32,63 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// const numSteps = 20.0;
+
+// let boxElement;
+// let prevRatio = 0.0;
+
+// // Set things up
+// window.addEventListener(
+//   "load",
+//   (event) => {
+//     boxElement = document.querySelector(".home__container");
+
+//     createObserver();
+//   },
+//   false
+// );
+
+// function createObserver() {
+//   let observer;
+
+//   let options = {
+//     root: null,
+//     rootMargin: "0px",
+//     threshold: buildThresholdList(),
+//     trackVisibility: true,
+//   };
+
+//   observer = new IntersectionObserver(handleIntersect, options);
+//   observer.observe(boxElement);
+// }
+
+// function buildThresholdList() {
+//   let thresholds = [];
+//   let numSteps = 20;
+
+//   for (let i = 1.0; i <= numSteps; i++) {
+//     let ratio = i / numSteps;
+//     thresholds.push(ratio);
+//   }
+
+//   thresholds.push(0);
+//   return thresholds;
+// }
+
+// function handleIntersect(entries, observer) {
+//   entries.forEach((entry) => {
+//     if (entry.intersectionRatio > prevRatio) {
+//       entry.target.style.opacity = entry.intersectionRatio;
+//     } else {
+//       entry.target.style.opacity = entry.intersectionRatio;
+//     }
+//     prevRatio = entry.intersectionRatio;
+//   });
+// }
